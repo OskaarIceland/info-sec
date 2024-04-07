aea50a8114f74ea88d6c59c093ddc69dafc8325c
Oscar  NoNeed <oscar.lindsay@gmail.com>
2024-04-07 22:58:54 +0000

    i give up at this point

--- myApp.js
+++ myApp.js
@@ -55,8 +55,9 @@ const helmet = require('helmet');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.use(helmet.hidePoweredBy());
+
 app.get("/", function (request, response) {
+  app.use(helmet.hidePoweredBy())
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
