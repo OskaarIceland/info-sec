493dae2a014652d14b79522d43889300011e286d
Oscar  NoNeed <oscar.lindsay@gmail.com>
2024-04-07 23:03:58 +0000

    last try

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet')
-app.use(helmet.hidePoweredBy());
+app.use(helmet.hidePoweredBy({
+  setTo: 'PHP 4.2.0'
+}));
 
 
 
